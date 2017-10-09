47573f2df540f4654b2a75348a7ba452f7642d67
ryumix22 <dimac2278@yandex.ru>
2017-10-09 22:15:36 +0300

    amended all lessons

--- test/lesson3/task1/Tests.kt
+++ test/lesson3/task1/Tests.kt
@@ -202,7 +202,9 @@ class Tests {
         assertEquals(1, squareSequenceDigit(1))
         assertEquals(4, squareSequenceDigit(2))
         assertEquals(5, squareSequenceDigit(7))
-        assertEquals(4, squareSequenceDigit(10))
+        assertEquals(6, squareSequenceDigit(12))
+        assertEquals(0, squareSequenceDigit(17))
+        assertEquals(9, squareSequenceDigit(27))
     }
 
     @Test
